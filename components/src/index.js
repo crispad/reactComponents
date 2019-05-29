ce1af6698aa83e1b5ce69855126dc2cba3677d19
Cristina Laryea <Number2@Georges-MacBook-Air.local>
2019-05-29 14:35:53 -0400

    cards displaying nicely with buttons

--- components/src/index.js
+++ components/src/index.js
@@ -7,22 +7,29 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-           <CommentDetail  
-                author="Sam" 
-                timeAgo="Today at 3:00PM" 
-                blogPost="Great hike up Mountain Bear!"
-                avatar={faker.image.avatar()} />
-           <CommentDetail  
-                author="Alex" 
-                timeAgo="Yesterday at 4:00PM" 
-                blogPost="Fun times with the family!"
-                avatar={faker.image.avatar()} />
-           <CommentDetail 
-                author="Jane" 
-                timeAgo="Yesterday at 2:07PM" 
-                blogPost="Today was a great day at the park!"
-                avatar={faker.image.avatar()} />
+            <ApprovalCard>
+                <CommentDetail  
+                    author="Sam" 
+                    timeAgo="Today at 3:00PM" 
+                    blogPost="Great hike up Mountain Bear!"
+                    avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail  
+                        author="Alex" 
+                        timeAgo="Yesterday at 4:00PM" 
+                        blogPost="Fun times with the family!"
+                        avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail 
+                        author="Jane" 
+                        timeAgo="Yesterday at 2:07PM" 
+                        blogPost="Today was a great day at the park!"
+                        avatar={faker.image.avatar()} />
+            </ApprovalCard>
         </div>
     );
 };
