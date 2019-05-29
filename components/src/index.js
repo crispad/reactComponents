b7248735d07d24fdaf8a1017e5bd558565feb4e4
Cristina Laryea <Number2@Georges-MacBook-Air.local>
2019-05-29 10:33:43 -0400

    time and blogs rendering on local site

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-           <CommentDetail  author="Sam" />
-           <CommentDetail  author="Alex" />
-           <CommentDetail author="Jane" />
+           <CommentDetail  author="Sam" timeAgo="Today at 3:00PM" blogPost="Great hike up Mountain Bear!"/>
+           <CommentDetail  author="Alex" timeAgo="Yesterday at 4:00PM" blogPost="Fun times with the family!"/>
+           <CommentDetail author="Jane" timeAgo="Yesterday at 2:07PM" blogPost="Today was a great day at the park!"/>
         </div>
     );
 };
