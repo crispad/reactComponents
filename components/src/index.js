b97c567d46dfd47970fc72f47f2a6aad4b28ef56
Cristina Laryea <Number2@Georges-MacBook-Air.local>
2019-05-29 11:42:36 -0400

    added avatar

--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import faker from 'faker';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-           <CommentDetail  author="Sam" timeAgo="Today at 3:00PM" blogPost="Great hike up Mountain Bear!"/>
-           <CommentDetail  author="Alex" timeAgo="Yesterday at 4:00PM" blogPost="Fun times with the family!"/>
-           <CommentDetail author="Jane" timeAgo="Yesterday at 2:07PM" blogPost="Today was a great day at the park!"/>
+           <CommentDetail  
+                author="Sam" 
+                timeAgo="Today at 3:00PM" 
+                blogPost="Great hike up Mountain Bear!"
+                avatar={faker.image.avatar()} />
+           <CommentDetail  
+                author="Alex" 
+                timeAgo="Yesterday at 4:00PM" 
+                blogPost="Fun times with the family!"
+                avatar={faker.image.avatar()} />
+           <CommentDetail 
+                author="Jane" 
+                timeAgo="Yesterday at 2:07PM" 
+                blogPost="Today was a great day at the park!"
+                avatar={faker.image.avatar()} />
         </div>
     );
 };
